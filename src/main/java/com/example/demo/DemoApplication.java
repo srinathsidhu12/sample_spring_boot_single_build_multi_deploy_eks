0c71fe5a3cb8c2149aeb19541efa854ad28e4da6
root <root@ip-172-31-33-199.ap-south-1.compute.internal>
2025-09-01 18:17:57 +0000

    modified Demoapplication.java file

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,11 +2,15 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class DemoApplication {
-   public static void main(String[] args) {
-       SpringApplication.run(DemoApplication.class, args);
-   }
+    public static void main(String[] args) {
+        ConfigurableApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
+        System.out.println("Active profiles: " + Arrays.toString(ctx.getEnvironment().getActiveProfiles()));
+        System.out.println("app.message = " + ctx.getEnvironment().getProperty("app.message"));
+    }
 }
 
